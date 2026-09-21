d056a71ade5ae2b2cfe6a28f977a1fcf74f561c9
srilakshmireddy1617-ctrl <srilakshmireddy1617@gmail.com>
2026-09-21 10:54:17 +0530

    Add files via upload

---
+++ heart_disease_sql.sql
@@ -0,0 +1,17 @@
+create database heart_analysis;
+use heart_analysis;
+select * from heart_disease_risk_2026;
+SELECT patient_id, age, sex, cholesterol_total, bmi, has_heart_disease
+FROM heart_disease_risk_2026;
+SELECT AVG(age) AS average_age
+FROM heart_disease_risk_2026;
+SELECT * FROM heart_disease_risk_2026
+WHERE has_heart_disease = 1;	
+SELECT COUNT(*) AS null_age_count
+FROM heart_disease_risk_2026
+WHERE age IS NULL;
+SELECT
+    smoker_status,
+    COUNT(*) AS patient_count
+FROM heart_disease_risk_2026
+GROUP BY smoker_status;
